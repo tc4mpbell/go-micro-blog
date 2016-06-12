3254031882261fae94a5168d157963d8adb1416e
Taylor Campbell <taylor.campbell@rtslabs.com>
2016-06-12 09:04:45 -0400

    init commit; based on Go http-server tut so far

---
+++ hello.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+    "errors"
+)
+
+/* GLOBAL */
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error)  {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+        http.NotFound(w, r)
+        return  "", errors.New("Invalid Page Title")
+    }
+    return m[2], nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Post) {
+	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+	if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+/* POSTS */
+
+type Post struct {
+	Title string
+	Body []byte
+}
+
+func path(filename string) string {
+	return "posts/" + filename + ".txt"
+}
+
+func (p* Post) save() error {
+	filename := path(p.Title)
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPost(title string) (*Post, error) {
+  filename := path(title)
+  body, err := ioutil.ReadFile(filename)
+  if err != nil {
+  	return nil, err
+  }
+  return &Post{Title: title, Body: body}, nil
+}
+
+/* WEB SERVER */
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
+
+    p, err := loadPost(title)
+
+    if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+    }
+    
+    renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
+    
+    p, err := loadPost(title)
+    if err != nil {
+        p = &Post{Title: title}
+    }
+    renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
+    
+    body := r.FormValue("body")
+    p := &Post{Title: title, Body: []byte(body)}
+    err = p.save()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+
